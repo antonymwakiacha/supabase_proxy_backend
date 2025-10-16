2d645b4be1663bfe7997c6718357853be37a38e7
antonymwakiacha <tonywugoma@gmail.com>
2025-10-16 09:42:08 +0300

    refactoring

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-t9y^qs5$u&w9s_y1p$0wb9y=_9w9)*(3!ym+4t06dlys63*d!8'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
